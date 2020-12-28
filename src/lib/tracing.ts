7f6ca7601a18375dbe2c17f889ef309c19fa5319
Myrotvorets <support@myrotvorets.center>
2020-12-28 19:48:28 +0200

    Update dependencies

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -1,30 +1,21 @@
 /* istanbul ignore file */
 
-import { NodeTracerProvider } from '@opentelemetry/node';
-import { SimpleSpanProcessor } from '@opentelemetry/tracing';
-import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 import { EventEmitter } from 'events';
+import { OpenTelemetryConfigurator } from '@myrotvorets/opentelemetry-configurator';
 
-const provider = new NodeTracerProvider({
-    plugins: {
-        express: {},
-        http: {},
-        https: {},
-        knex: {
-            path: '@myrotvorets/opentelemetry-plugin-knex',
+const configurator = new OpenTelemetryConfigurator({
+    serviceName: 'psb-api-dzhura',
+    tracer: {
+        plugins: {
+            express: {},
+            http: {},
+            https: {},
+            knex: {
+                path: '@myrotvorets/opentelemetry-plugin-knex',
+            },
         },
     },
 });
 
-if (+(process.env.ENABLE_TRACING || 0) && process.env.ZIPKIN_ENDPOINT) {
-    const zipkinExporter = new ZipkinExporter({
-        url: process.env.ZIPKIN_ENDPOINT,
-        serviceName: 'psb-api-dzhura',
-    });
-
-    const zipkinProcessor = new SimpleSpanProcessor(zipkinExporter);
-    provider.addSpanProcessor(zipkinProcessor);
-}
-
-provider.register();
+configurator.start().catch((e) => console.error('Failed to configure OpenTelemetry:', e));
 EventEmitter.defaultMaxListeners = 12;
