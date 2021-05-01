ee5321a06b2e205e73c08bb0446c8a05240782ad
Myrotvorets <support@myrotvorets.center>
2021-05-01 03:15:55 +0300

    Update dependencies

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -2,19 +2,12 @@
 
 import { EventEmitter } from 'events';
 import { OpenTelemetryConfigurator } from '@myrotvorets/opentelemetry-configurator';
+// import { KnexPlugin } from '@myrotvorets/opentelemetry-plugin-knex';
 
 if (+(process.env.ENABLE_TRACING || 0)) {
     const configurator = new OpenTelemetryConfigurator({
         serviceName: 'psb-api-dzhura',
-        instrumentations: [
-            {
-                plugins: {
-                    knex: {
-                        path: '@myrotvorets/opentelemetry-plugin-knex',
-                    },
-                },
-            },
-        ],
+        // instrumentations: [new KnexPlugin()],
     });
 
     configurator.start().catch((e) => console.error('Failed to initialize OpenTelemetry:', e));
